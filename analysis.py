1ddba6110147ae5bf7b7bcc265a5111908e0114b
michael-p-sachen <michael.p.sachen@gmail.com>
2023-12-07 20:23:52 -0500

    clean up

--- analysis.py
+++ analysis.py
@@ -20,17 +20,16 @@ def do_analysis(gaze_data_path, image_path):
 
     data = pd.read_csv(gaze_data_path)
 
-    x = np.array((data['Right_X'] + data['Left_X']) / 2)
-    y = np.array((data['Right_Y'] + data['Left_Y']) / 2)
+    x = np.array((data['Right_X'] + data['Left_X']) / 2) * 1440
+    y = np.array((data['Right_Y'] + data['Left_Y']) / 2) * 900
     timestamps = np.array(data['Timestamp'])
 
-    _, fixations = detectors.fixation_detection(x, y, timestamps, mindur=20)
-    _, saccades = detectors.saccade_detection(x, y, timestamps, minlen=.1)
+    _, fixations = detectors.fixation_detection(x, y, timestamps)
+    _, saccades = detectors.saccade_detection(x, y, timestamps)
 
     num_fixations = len(fixations)
     mean_fixation_duration = np.mean([fix[2] for fix in fixations])
     sd_fixation_duration = np.std([fix[2] for fix in fixations])
-
     num_saccades = len(saccades)
     mean_saccade_length = np.mean([sacc[2] for sacc in saccades])
     sd_saccade_length = np.std([sacc[2] for sacc in saccades])
@@ -47,4 +46,4 @@ def do_analysis(gaze_data_path, image_path):
 
 
 if __name__ == '__main__':
-    do_analysis('./gaze_data.csv', './sample.jpeg')
+    do_analysis('./raw_gaze_data.csv', './sample.jpeg')
